1d257152da31503ad456e819808d90a964199f12
ces518 <pupupee9@gmail.com>
2019-08-10 23:52:34 +0900

    Event 생성 API 구현 - EventValidator

---
+++ src/main/java/me/june/restapi/events/EventValidator.java
@@ -0,0 +1,32 @@
+package me.june.restapi.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 2019-08-10
+ * Time: 23:25
+ **/
+@Component
+public class EventValidator{
+
+    public void validate (EventDto eventDto, Errors errors) {
+        // 무제한 경매가 아닌데, basePrice 가 max보다 큰 경우
+        if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
+            errors.rejectValue("basePrice", "wrongValue", "BasePrice is must be less than MaxPrice");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+        endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+        endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "EndEventDateTime is must be more than begin");
+        }
+
+    }
+}
