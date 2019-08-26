c5eaf243c652c5ddf4993f5592ba7b28eb114eec
ces518 <pupupee9@gmail.com>
2019-08-26 22:19:17 +0900

    RESTAPI - OAuthServerConfigTest

---
+++ src/test/java/me/june/restapi/configs/OAuthServerConfigTest.java
@@ -0,0 +1,58 @@
+package me.june.restapi.configs;
+
+
+import me.june.restapi.accounts.Account;
+import me.june.restapi.accounts.AccountRole;
+import me.june.restapi.accounts.AccountService;
+import me.june.restapi.common.BaseControllerTest;
+import me.june.restapi.common.TestDescription;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class OAuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @TestDescription("인증 토큰을 발급 받는 테스트")
+    public void getAuthToken () throws Exception {
+
+        final String USER_NAME = "puppee9@gmail.com";
+        final String PASSWORD = "june";
+        // httpBasic 메서드를 사용하여 basicOauth 헤더를 만듬
+        final String CLIENT_ID = "myApp";
+        final String CLIENT_SECRET = "pass";
+
+        // given
+        Set roles = new HashSet();
+        roles.add(AccountRole.ADMIN);
+        roles.add(AccountRole.USER);
+        Account account = Account.builder()
+                .email(USER_NAME)
+                .password(PASSWORD)
+                .roles(roles)
+                .build();
+        accountService.saveAccount(account);
+
+        this.mockMvc.perform(post("/oauth/token")
+                    .with(httpBasic(CLIENT_ID, CLIENT_SECRET)) // httpBasic 사용시 test dependency 필요
+                    .param("username", USER_NAME)
+                    .param("password", PASSWORD)
+                    .param("grant_type", "password")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.access_token").exists());
+    }
+
+}
