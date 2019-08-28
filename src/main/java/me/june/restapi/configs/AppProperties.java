336dd03fe55b4b1936d32d3546132908cf6fe1f5
ces518 <pupupee9@gmail.com>
2019-08-28 23:07:11 +0900

    RESTAPI - AppProperties 외부설정

---
+++ src/main/java/me/june/restapi/configs/AppProperties.java
@@ -0,0 +1,38 @@
+package me.june.restapi.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 2019-08-28
+ * Time: 22:26
+ **/
+@Component
+@ConfigurationProperties(prefix = "my-app")
+@Getter @Setter
+public class AppProperties {
+
+    @NotEmpty
+    private String adminUsername;
+
+    @NotEmpty
+    private String adminPassword;
+
+    @NotEmpty
+    private String userUsername;
+
+    @NotEmpty
+    private String userPassword;
+
+    @NotEmpty
+    private String clientId;
+
+    @NotEmpty
+    private String clientSecret;
+}
