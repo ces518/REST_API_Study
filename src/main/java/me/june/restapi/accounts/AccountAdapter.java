31c77093d378a0107fbf0bebe0c1cb8a3c6399fb
ces518 <pupupee9@gmail.com>
2019-08-29 22:48:05 +0900

    RESTAPI - AccountAdapter 클래스 생성

---
+++ src/main/java/me/june/restapi/accounts/AccountAdapter.java
@@ -0,0 +1,33 @@
+package me.june.restapi.accounts;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 2019-08-29
+ * Time: 21:59
+ **/
+@Getter
+public class AccountAdapter extends User {
+
+    private Account account;
+
+    public AccountAdapter(Account account) {
+        super(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+        this.account = account;
+    }
+
+    private static Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toSet());
+    }
+}
